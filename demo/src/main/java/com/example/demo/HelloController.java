9e4180f27f80bb6d19fbaba8fdc4d4373c84df48
Trash-panda330 <uetak330@gmail.com>
2024-09-08 23:32:44 +0900

    Githubの連携確認がしたい

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class HelloController {
+	@RequestMapping(value="/hello")
+		private String hello() {
+			return "hello.html";
+		}
+	
 }
