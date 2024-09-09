96590eca549462884a89d7db6b3ac2e438001e03
Trash-panda330 <uetak330@gmail.com>
2024-09-09 23:00:37 +0900

    ～

--- YesDI/src/main/java/com/example/demo/EmployeeController.java
+++ YesDI/src/main/java/com/example/demo/EmployeeController.java
@@ -10,36 +10,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 	//
 	private final EmployeeService employeeService;
-	
+
 	//EmployeeServiceのDI
 	public EmployeeController(EmployeeService employeeService) {
 		this.employeeService = employeeService;
 	}
 
-    @RequestMapping("/input")
-    public String index(EmployeeForm employeeForm) {
-        // "index.html" テンプレートを返す
-        return "index.html";
-    }
+	@RequestMapping("/input")
+	public String index(EmployeeForm employeeForm) {
+		// "index.html" テンプレートを返す
+		return "index.html";
+	}
 
-    @RequestMapping("/output")
-      public String result(@Validated EmployeeForm employeeForm, BindingResult bindingResult, Model model) {
-    	
-    	//errorがあったら"index.html"に戻す
-    	if(bindingResult.hasErrors()) {
-    		return "index.html";
-    	}
-    	
-       //依存性がない場合だとこのnewする部分がないとエラーになる
-//        EmployeeService employeeService = new EmployeeServiceImpl();
+	@RequestMapping("/output")
+	public String result(@Validated EmployeeForm employeeForm, BindingResult bindingResult, Model model) {
 
-    	
-        //サービス層から社員を検索
-        String name = employeeService.findByNo(employeeForm.getNumber());
+		//errorがあったら"index.html"に戻す
+		if (bindingResult.hasErrors()) {
+			return "index.html";
+		}
 
-        model.addAttribute("number", employeeForm.getNumber()); // モデルに「number」を追加
-        model.addAttribute("name", name);     // モデルに「name」を追加
-        // "output.html" テンプレートを返す
-        return "output.html";
-    }
+		//依存性がない場合だとこのnewする部分がないとエラーになる
+		//EmployeeService employeeService = new EmployeeServiceImpl();
+
+		//サービス層から社員を検索
+		String name = employeeService.findByNo(employeeForm.getNumber());
+
+		model.addAttribute("number", employeeForm.getNumber()); // モデルに「number」を追加
+		model.addAttribute("name", name); // モデルに「name」を追加
+		// "output.html" テンプレートを返す
+		return "output.html";
+	}
 }
