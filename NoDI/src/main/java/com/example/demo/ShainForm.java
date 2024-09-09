f3e21810f9599e66d0a64f5bf3915e4a65d155a9
Trash-panda330 <uetak330@gmail.com>
2024-09-09 13:06:14 +0900

    ShainFormクラスの作成
    フィールドとゲッターセッターの作成

---
+++ NoDI/src/main/java/com/example/demo/ShainForm.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class ShainForm {
+	
+	private String number;
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+}
