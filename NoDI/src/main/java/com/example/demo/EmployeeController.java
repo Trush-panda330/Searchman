261df66b5283f9a4d9fbee6a58a8c7129d25c4be
Trash-panda330 <uetak330@gmail.com>
2024-09-09 19:13:07 +0900

    EmployeeServiceの実装によるコントローラーの変更

--- NoDI/src/main/java/com/example/demo/EmployeeController.java
+++ NoDI/src/main/java/com/example/demo/EmployeeController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EmployeeController {
 
-    @RequestMapping("/input")
-    public String index(EmployeeForm employeeForm) {
-        // "index.html" テンプレートを返す
-        return "index.html";
-    }
+	@RequestMapping("/input")
+	public String index(EmployeeForm employeeForm) {
+		// "index.html" テンプレートを返す
+		return "index.html";
+	}
 
-    @RequestMapping("/output")
+	@RequestMapping("/output")
       public String result(@Validated EmployeeForm employeeForm, BindingResult bindingResult, Model model) {
     	
     	//errorがあったら"index.html"に戻す
@@ -24,7 +24,11 @@ public class EmployeeController {
     	}
     	
         // パラメータ「number」を取得し、「name」を設定する
-        String name = "コントローラー太郎";
+//        String name = "コントローラー太郎";
+        
+        EmployeeService employeeService = new EmployeeServiceImpl();
+        String name = employeeService.findByNo(employeeForm.getNumber());
+			
         model.addAttribute("number", employeeForm.getNumber()); // モデルに「number」を追加
         model.addAttribute("name", name);     // モデルに「name」を追加
         // "output.html" テンプレートを返す
