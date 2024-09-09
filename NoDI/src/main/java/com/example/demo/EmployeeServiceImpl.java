4bde489518d403f07a1d03950e0df7ad14fb99d6
Trash-panda330 <uetak330@gmail.com>
2024-09-09 17:10:03 +0900

    社員番号で社員を探す実装を持ったクラスを定義

---
+++ NoDI/src/main/java/com/example/demo/EmployeeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class EmployeeServiceImpl implements EmployeeService {
+	@Override
+	public String findByNo(String number) {
+		//リポジトリから社員を選択
+		EmployeeRepository employeeRepository = new EmployeeRepositoryImpl();
+		String name = employeeRepository.selectByNo(number);
+		return null;
+	}
+}
